c57eb8e13494a0eadf5ece56dd17082b04936d6b
celine <josephinecelinetj@gmail.com>
2025-08-22 13:48:38 +0800

    Add greeting

---
+++ src/main/java/Pip.java
@@ -0,0 +1,11 @@
+public class Pip {
+    public static void main(String[] args) {
+        String message = "____________________________________________________________ \n"
+                + "Hello! I'm Pip \n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "____________________________________________________________";
+        System.out.println(message);
+    }
+}
