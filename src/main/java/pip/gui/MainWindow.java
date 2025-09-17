3c27f1c9464f663eed3b5f9593eed83125c0edb1
celine <josephinecelinetj@gmail.com>
2025-09-18 01:44:16 +0800

    Make profile pictures smaller and circular

--- src/main/java/pip/gui/MainWindow.java
+++ src/main/java/pip/gui/MainWindow.java
@@ -1,12 +1,17 @@
 package pip.gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 /**
  * Controller for the main GUI.
@@ -14,6 +19,8 @@ import javafx.scene.layout.VBox;
 public class MainWindow extends AnchorPane {
     private static final String USER_IMG_PATH = "/images/user.jpeg";
     private static final String PIP_IMG_PATH = "/images/pip.jpg";
+
+    private static final double AVATAR_SIZE = 300;
 
     @FXML
     private ScrollPane scrollPane;
@@ -25,12 +32,20 @@ public class MainWindow extends AnchorPane {
     private Button sendButton;
 
     private Pip pip;
-    private Image userImage = new Image(this.getClass().getResourceAsStream(USER_IMG_PATH));
-    private Image pipImage = new Image(this.getClass().getResourceAsStream(PIP_IMG_PATH));
+    private Image rawUserImage = new Image(this.getClass().getResourceAsStream(USER_IMG_PATH));
+    private Image rawPipImage = new Image(this.getClass().getResourceAsStream(PIP_IMG_PATH));
 
+    private Image userImage;
+    private Image pipImage;
+
+    /**
+     * Initializes the main window after its FXML elements have been loaded.
+     * */
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        userImage = makeCircularSnapshot(rawUserImage, AVATAR_SIZE);
+        pipImage = makeCircularSnapshot(rawPipImage, AVATAR_SIZE);
     }
 
     /** Injects the Pip instance */
@@ -61,5 +76,24 @@ public class MainWindow extends AnchorPane {
                 DialogBox.getPipDialog(response, pipImage)
         );
         userInput.clear();
+    }
+
+    private static Image makeCircularSnapshot(Image src, double size) {
+        ImageView iv = new ImageView(src);
+        iv.setFitWidth(size);
+        iv.setFitHeight(size);
+        iv.setPreserveRatio(false);
+
+        Circle clip = new Circle(size / 2, size / 2, size / 2);
+        iv.setClip(clip);
+
+        SnapshotParameters params = new SnapshotParameters();
+        params.setFill(Color.TRANSPARENT);
+
+        WritableImage wi = new WritableImage((int) Math.ceil(size), (int) Math.ceil(size));
+        WritableImage snapshot = iv.snapshot(params, wi);
+
+        iv.setClip(null);
+        return snapshot;
     }
 }
